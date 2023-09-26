38c37643f867f3a28979532e011134a4dba9f187
Marcos Manoel Candido <marcosmanoelcandido@gmail.com>
2023-09-26 10:42:44 -0300

    ENQUANTO- estrutura de repetição

---
+++ EstudoEdu/Java/CursoAtividade.java
@@ -0,0 +1,4 @@
+public class CursoAtividade {
+    int idade = int;
+
+}
--- EstudoEdu/Java/Variaveis.java
+++ EstudoEdu/Java/Variaveis.java
@@ -1,6 +1,6 @@
 public class Variaveis {
- public static void main(String[] args) {
-    int idade = 29;
+ public static Variaveis main(String[] args) {
+    int idade = ;
     int idadeFuturo = 10 + idade;
     double altura = 1.80;
     boolean estudando = true;
